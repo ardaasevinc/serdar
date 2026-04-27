7387acc14c81c16277d0e2e6142c87c2e32ef87d
ardaasevinc <ardaasevinc@gmail.com>
2026-04-27 21:57:33 +0300

    test

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -1,5 +1,5 @@
 <div class="preloader">
-   <div class="preloader__image" style="background-image: url('{{ asset('uploads/' . $settings->dark_logo) }}');" width="200px;"></div>
+   <div class="preloader__image" style="background-image: url('{{ asset('uploads/' . $settings?->dark_logo) }}');" width="200px;"></div>
 
 </div>
 <!-- /.preloader -->
